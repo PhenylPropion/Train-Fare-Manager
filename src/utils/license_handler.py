f59dfbd9c7d980f387deddaaff0af239e795f344
霍永豪 <92239784+PhenylPropion@users.noreply.github.com>
2024-11-29 20:05:21 +0900

    Update license_handler.py

--- src/utils/license_handler.py
+++ src/utils/license_handler.py
@@ -4,6 +4,17 @@ import os
 
 def verify_license(license_key):
     # ライセンス検証ロジック
+    license_key = license_entry.get()
+
+    if check_date(license_key):  # ここに実際のライセンスキーを設定
+        with open(LICENSE_FILE, "w") as file:
+            file.write(license_key)
+        license_window.destroy()  # ライセンスウィンドウを閉じる
+        main()  # main関数を呼び出す
+
+        return True
+    else:
+        return False
     pass
 
 def save_license(license_key):
